33cc289dc0f61f0281ed25baa0a784361edc1c08
Muhammed Patel <patel.muhammed@gmail.com>
2023-12-21 01:23:12 +0200

    Cleaned up code added Services

--- AlertApi/Services/TwilioService.cs
+++ AlertApi/Services/TwilioService.cs
@@ -1,26 +1,31 @@
-using System;
+using Microsoft.Extensions.Configuration;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
-using Twilio.Types;
-namespace Services{
+
 public class TwilioService
 {
-    public static MessageResource sendSMS(string phoneNumber)
-    {
-        var accountSid = "AC3da6b1f0e3c57033107e3539de9d56d0";
-        var authToken = "ba765d018f5997fc877b2643b892c88e";
-        TwilioClient.Init(accountSid, authToken);
+  private readonly IConfiguration config;
 
-        var messageOptions = new CreateMessageOptions(
-          new PhoneNumber(phoneNumber));
-        messageOptions.From = new PhoneNumber("+12058597340");
-        messageOptions.Body = "Hello from Secure Citizen!";
+  public TwilioService(IConfiguration configuration)
+  {
+    config = configuration;
+  }
 
+  public  MessageResource sendSMS(string phoneNumber)
+  {
+    var accountSid = config["Twilio:accountSID"];
+    var authToken = config["Twilio:authToken"];
+    var fromPhoneNumber = config["Twilio:fromNumber"];
 
-        var message = MessageResource.Create(messageOptions);
-        Console.WriteLine(message.Body);
+    TwilioClient.Init(accountSid, authToken);
 
-        return message;
-    }
-}
+    var messageOptions = new CreateMessageOptions(new Twilio.Types.PhoneNumber(phoneNumber));
+    messageOptions.From = new Twilio.Types.PhoneNumber(fromPhoneNumber);
+    messageOptions.Body = "Hello from Secure Citizen!";
+
+    var message = MessageResource.Create(messageOptions);
+    Console.WriteLine(message.Body);
+
+    return message;
+  }
 }
